73a0c1af32435177b50c6e5c38d8554a2876a83b
Ichabod Crane <beboldb@hotmail.com>
2023-09-01 01:22:01 -0400

    solutions

---
+++ Warmup-1/posNeg.java
@@ -0,0 +1,9 @@
+public boolean posNeg(int a, int b, boolean negative){
+	if((negative != true) && ((a < 0 && b > 0) || (a > 0 && b < 0))){
+		return true;
+	}
+	if((negative != false) && ((a < 0) && (b < 0))){
+		return true;
+	}
+	return false;
+}
