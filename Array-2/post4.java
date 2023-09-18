de59caa9a187af92b79f563bad8597509ea9cd1b
Ichabod Crane <beboldb@hotmail.com>
2023-09-17 23:19:33 -0400

    Update post4.java

--- Array-2/post4.java
+++ Array-2/post4.java
@@ -4,18 +4,22 @@ public int[] post4(int[] nums){
     int idx = 0;
 
     for(int i = 0; i < nums.length; i++){
+        //check if current element is a 4;
         if(nums[i] == 4){
             seenFour = true;
             idx = i;
         }
+        //check if current element is a four and we've seen a four;
         if(nums[i] != 4 && seenFour){
             count++;
         }else{
             count = 0;
         }
     }
+    //create new Array size count;
     int[] nA = new int[count];
     for(int i = idx + 1, j = 0; i < nums.length; i++){
+        //populate new array with every number after last four seen;
         nA[j] = nums[i];
         j++;
     }
