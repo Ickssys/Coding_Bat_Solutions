6b9729c6ddc5f568684069a4bf499e4a119a5a01
Ichabod Crane <beboldb@hotmail.com>
2023-10-05 10:01:23 -0400

    Create either24.java

---
+++ Array-2/either24.java
@@ -0,0 +1,13 @@
+public static boolean either24(int[] nums){
+       boolean seen22 = false; 
+       boolean seen44 = false;
+       for(int i = 0; i < nums.length - 1; i++){
+        if(nums[i] == 2 && nums[i+1] == nums[i]){
+            seen22 = true;
+        }
+        if(nums[i] == 4 && nums[i+1] == nums[i]){
+            seen44 = true;
+        }
+       }
+       return seen22 != seen44;
+    }
