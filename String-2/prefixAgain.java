3bdb9094e49bb308b1923230652d7d761080e3cd
Ichabod Crane <beboldb@hotmail.com>
2023-11-14 23:28:04 -0500

    Update prefixAgain.java

--- String-2/prefixAgain.java
+++ String-2/prefixAgain.java
@@ -1,6 +1,6 @@
 public boolean prefixAgain(String str, int n){
 	String prefix = str.substring(0,n);
-	for(int i = 0; i <= str.length() - n; i++){
+	for(int i = n; i <= str.length() - n; i++){
 		if(prefix.equals(str.substring(i, i + n))){
 			return true;
 		}
