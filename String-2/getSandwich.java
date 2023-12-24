47d6f2175a99d2da140875d27983a3e251f9989f
Ichabod Crane <beboldb@hotmail.com>
2023-12-23 20:09:10 -0500

    Create getSandwich.java

---
+++ String-2/getSandwich.java
@@ -0,0 +1,6 @@
+public static String getSandwich(String str){
+	if(str.indexOf("bread") == str.lastIndexOf("bread")){
+            return "";
+        }    
+        return str.substring(str.indexOf("bread") + 5, str.lastIndexOf("bread");
+}
