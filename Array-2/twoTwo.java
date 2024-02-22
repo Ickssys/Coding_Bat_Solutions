6676e27b09d616c36361b654736e7f838cfdda99
Carlos Perez <beboldb@hotmail.com>
2024-02-21 21:42:20 -0500

    modify twoTwo.java

--- Array-2/twoTwo.java
+++ Array-2/twoTwo.java
@@ -1,14 +1,14 @@
 public static boolean twoTwo(int[] nums){
-    int isTwoCount = 0, isTwoNext = 0;
-    boolean containsTwo = false;
-    for(int i = 0; i < nums.length; i++){
-       if(nums[i] == 2){
-          containsTwo = true;
-          isTwoCount++;
-          if(i < nums.length - 1 && nums[i + 1] == 2){
-             isTwoNext++;
-          }
-       }
-    }
-    return (isTwoCount - isTwoNext == 1  && isTwoNext != 0) || !containsTwo;
- }
+   int isTwoCount = 0, isTwoNext = 0;
+   boolean containsTwo = false;
+   for(int i = 0; i < nums.length; i++){
+      if(nums[i] == 2){
+         containsTwo = true;
+         isTwoCount++;
+         if(i < nums.length - 1 && nums[i + 1] == 2){
+         isTwoNext++;
+         }
+      }
+   }
+   return (isTwoCount - isTwoNext == 1  && isTwoNext != 0) || !containsTwo;
+}
